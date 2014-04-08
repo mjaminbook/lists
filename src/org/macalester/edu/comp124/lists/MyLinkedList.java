11ee3fd66b469bc63f5c12eb66139a6989ed1b8a
Benjamin Mathers <bmathers@macalester.edu>
2014-04-07 20:31:51 -0500

    Hide and Seek
    Project
    
    lab

--- src/org/macalester/edu/comp124/lists/MyLinkedList.java
+++ src/org/macalester/edu/comp124/lists/MyLinkedList.java
@@ -1,81 +1,219 @@
-package org.macalester.edu.comp124.lists;
+import org.macalester.edu.comp124.lists.MyLinkedNode;
 
 /**
+
  * An unfinished implementation of a basic doubly-linked list.
+
  *
+
  * Note that the head and the tail contain "sentinel" nodes without values.
+
  * This makes the edge cases of the algorithm easier because the list always has a discrete head and tail.
+
  *
+
  * @author shilad
+
  * @param <E>
+
  */
+
 public class MyLinkedList<E> {
-	private MyLinkedNode<E> head;
-	private MyLinkedNode<E> tail;
-	private int numElements = 0;
+
+    private MyLinkedNode<E> head;
+
+    private MyLinkedNode<E> tail;
+
+    private int numElements = 0;
+
+
 
     /**
+
      * Creates a new empty linked list.
+
      */
-	public MyLinkedList() {
-		head = new MyLinkedNode<E>(null);
-		tail = new MyLinkedNode<E>(null);
-		clear();
-	}
+
+    public MyLinkedList() {
+
+        head = new MyLinkedNode<E>(null);
+
+        tail = new MyLinkedNode<E>(null);
+
+        clear();
+
+    }
+
+
 
     /**
+
      * Returns the element at position index.
+
      * @param index
+
      * @return
+
      */
-	public E get(int index) {
-		return null;
-	}
+
+    public E get(int index) {
+
+        MyLinkedNode<E> node = head;
+
+        for (int j = 0; j <= index; j++)
+
+        {
+
+            node = node.getNext();
+
+        }
+
+
+
+        return node.getValue();
+
+    }
+
+
 
     /**
+
      * Adds a new element to the end of the list:
+
      *
+
      * @param elem
+
      */
-	public void add(E elem) {
-	}
+
+    public void add(E elem) {
+
+        MyLinkedNode<E> newNode = new MyLinkedNode(elem);
+
+        newNode.setNext(tail);
+
+        MyLinkedNode<E> secondToLast = tail.getPrev();
+
+        secondToLast.setNext(newNode);
+
+        tail.setPrev(newNode);
+
+        newNode.setPrev(secondToLast);
+
+
+
+        numElements++;
+
+    }
+
+
 
     /**
+
      * Inserts a new element at the specified index.
+
      *
+
      * @param elem
+
      */
-	public void add(int i, E elem) {
-	}
+
+    public void add(int i, E elem) {
+
+        MyLinkedNode<E> current = head;
+
+        for (int j = 0; j <= i; j++)
+
+        {
+
+            current = current.getNext();
+
+        }
+
+
+
+        MyLinkedNode<E> prevNode = current.getPrev();
+
+        MyLinkedNode<E> newNode = new MyLinkedNode(elem);
+
+
+
+        newNode.setNext(current);
+
+        newNode.setPrev(prevNode);
+
+        prevNode.setNext(newNode);
+
+        current.setPrev(newNode);
+
+
+
+        numElements++;
+
+
+
+    }
+
+
 
     /**
+
      * Returns the current size of the list.
+
      * @return
+
      */
-	public int size() {
-		return numElements;
-	}
+
+    public int size() {
+
+        return numElements;
+
+    }
+
+
 
     /**
+
      * Clears the contents of the list.
+
      */
-	public void clear() {
-		// set head's next to the tail
-		head.setNext(tail);
-		head.setPrev(null);
-		
-		// set tails prev to the head
-		tail.setPrev(head);
-		tail.setNext(null);
-		numElements = 0;
-	}
+
+    public void clear() {
+
+// set head's next to the tail
+
+        head.setNext(tail);
+
+        head.setPrev(null);
+
+
+// set tails prev to the head
+
+        tail.setPrev(head);
+
+        tail.setNext(null);
+
+        numElements = 0;
+
+    }
+
+
+
 
 
     /**
+
      * These methods are only used by the unit tests.
+
      * They are intentionally not public.
+
      * @return
+
      */
+
     MyLinkedNode getHead() { return head; }
+
     MyLinkedNode getTail() { return tail; }
+
 }
+
